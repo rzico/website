562420659e09ea1aa2413aae0cab54ee4947aacc
zhangsr <zhangsr@tiaohuo.com>
2017-10-19 11:39:54 +0800

    init

--- website/containers/article/header.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="header">
-        <h1 class="title">㲻孩子气？出版自由贸易区谈判解决伊朗核</h1>
-    </div>
-</template>
-<script>
-    export default {}
-</script>
--- website/containers/article/meta.vue
+++ website/containers/article/meta.vue
@@ -1,4 +1,7 @@
 <template>
+    <div class="header">
+        <h1 class="title">㲻孩子气？出版自由贸易区谈判解决伊朗核</h1>
+    </div>
     <div class="meta clearfix">
         <span>2017-10-01</span>
         <span class="nickname"><a class="toappuser" href="https://www.meipian.cn/c/17580620">张森荣</a></span>
