e5abca8facd4b3d65b60f5e4108adcac2c6d8c30
张功洋 <1260438719@qq.com>
2017-12-22 11:54:32 +0800

    a

---
+++ src/page/member/111.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span>111</span>
+  <!--<card ref="card" ></card>-->
+  <!--<couponList ref="couponList"></couponList>-->
+  </div>
+</template>
+<script>
+  import utils from '../../assets/utils.js';
+  import card from './card.vue';
+  import couponList from './couponList.vue';
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      card,
+      couponList,
+    },
+  }
+  </script>
