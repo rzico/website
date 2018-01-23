12c3f9b914a12fda6b8a7f0a1fc32f84fd31436f
张功洋 <1260438719@qq.com>
2018-01-23 21:27:59 +0800

    a

--- src/widget/tabbar-whole.vue
+++ src/widget/tabbar-whole.vue
@@ -11,6 +11,12 @@
         </span>
       <!--</a>-->
       </div>
+      <div  class="weui-tabbar__item"  :class="[id == 3 ? 'weui-bar__item_on':'']" @click="goshop()">
+        <span style="display: inline-block;position: relative;">
+          <span class="weui-tabbar__icon fftIcon" :style="{fontFamily:'iconfont'}">&#xe603;</span>
+          <p class="weui-tabbar__label">商品</p>
+        </span>
+      </div>
       <div  class="weui-tabbar__item"  :class="[id == 1 ? 'weui-bar__item_on':'']" @click="goorderList()">
         <span style="display: inline-block;position: relative;">
           <span class="weui-tabbar__icon fftIcon" :style="{fontFamily:'iconfont'}">&#xe600;</span>
@@ -150,6 +156,11 @@
       goorderList:function () {
         var vars  =  utils.router(location.href);
         vars.name ="orderList";
+        this.$router.push(vars);
+      },
+      goshop:function () {
+        var vars  =  utils.router(location.href);
+        vars.name ="goodsList";
         this.$router.push(vars);
       },
       open(){
