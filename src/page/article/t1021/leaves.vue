50f9962d7a1f6fd8e3a7e4eff4a40d41de3489fa
kezhijie <453471869@qq.com>
2018-05-20 11:33:51 +0800

    kzj 1021 1022网络图改成本地图

--- src/page/article/t1021/leaves.vue
+++ src/page/article/t1021/leaves.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <div class="snow1" style="width: 20.0477px; position: fixed; left: 251.706px; animation-name: toright; top: -30px; animation-duration: 23.4094s; animation-iteration-count: infinite; animation-timing-function: linear; z-index: 11;animation-delay:7.5s">
-      <img src="https://ss2.meipian.me/theme/v2/img/136/leaf3@2x.png" style="animation-name: rotatez; animation-duration: 4.05309s; animation-iteration-count: infinite; animation-timing-function: ease-in-out; animation-direction: alternate; width: 100%; height: 100%;">
+      <img src="../../../img/t1021/1021_leaf3@2x.png" style="animation-name: rotatez; animation-duration: 4.05309s; animation-iteration-count: infinite; animation-timing-function: ease-in-out; animation-direction: alternate; width: 100%; height: 100%;">
     </div>
     <div class="snow1" style="width: 18.4886px; position: fixed; left: 204.953px; animation-name: toleft; top: -30px; animation-duration: 21.5666s; animation-iteration-count: infinite; animation-timing-function: linear; z-index: 11;animation-delay:11s">
-      <img src="https://ss2.meipian.me/theme/v2/img/136/leaf2@2x.png" style="animation-name: rotatez; animation-duration: 5.44258s; animation-iteration-count: infinite; animation-timing-function: ease-in-out; animation-direction: alternate; width: 100%; height: 100%;">
+      <img src="../../../img/t1021/1021_leaf2@2x.png" style="animation-name: rotatez; animation-duration: 5.44258s; animation-iteration-count: infinite; animation-timing-function: ease-in-out; animation-direction: alternate; width: 100%; height: 100%;">
     </div>
     <div class="snow1" style="width: 22.8063px; position: fixed; left: 270.727px; animation-name: toleft; top: -30px; animation-duration: 22.5827s; animation-iteration-count: infinite; animation-timing-function: linear; z-index: 11;animation-delay:3.5s">
-      <img src="https://ss2.meipian.me/theme/v2/img/136/leaf1@2x.png" style="animation-name: rotatez; animation-duration: 4.90225s; animation-iteration-count: infinite; animation-timing-function: ease-in-out; animation-direction: alternate; width: 100%; height: 100%;">
+      <img src="../../../img/t1021/1021_leaf1@2x.png" style="animation-name: rotatez; animation-duration: 4.90225s; animation-iteration-count: infinite; animation-timing-function: ease-in-out; animation-direction: alternate; width: 100%; height: 100%;">
     </div>
     <div class="snow1" style="width: 21.1676px; position: fixed; left: 222.663px; animation-name: toleft; top: -30px; animation-duration: 20.8085s; animation-iteration-count: infinite; animation-timing-function: linear; z-index: 11;animation-delay:15s">
-      <img src="https://ss2.meipian.me/theme/v2/img/136/leaf3@2x.png" style="animation-name: rotatez; animation-duration: 3.17725s; animation-iteration-count: infinite; animation-timing-function: ease-in-out; animation-direction: alternate; width: 100%; height: 100%;">
+      <img src="../../../img/t1021/1021_leaf3@2x.png" style="animation-name: rotatez; animation-duration: 3.17725s; animation-iteration-count: infinite; animation-timing-function: ease-in-out; animation-direction: alternate; width: 100%; height: 100%;">
     </div>
     <div class="snow1" style="width: 23.9885px; position: fixed; left: 70.1701px; animation-name: toright; top: -30px; animation-duration: 23.6339s; animation-iteration-count: infinite; animation-timing-function: linear; z-index: 11;animation-delay:6s">
-      <img src="https://ss2.meipian.me/theme/v2/img/136/leaf1@2x.png" style="animation-name: rotatez; animation-duration: 3.06754s; animation-iteration-count: infinite; animation-timing-function: ease-in-out; animation-direction: alternate; width: 100%; height: 100%;">
+      <img src="../../../img/t1021/1021_leaf1@2x.png" style="animation-name: rotatez; animation-duration: 3.06754s; animation-iteration-count: infinite; animation-timing-function: ease-in-out; animation-direction: alternate; width: 100%; height: 100%;">
     </div>
   </div>
 </template>
